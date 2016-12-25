0c3e5b1f087355e57b0d4fb7e453cbd01fe70955
William Rudenmalm <me@whn.se>
2016-12-25 19:38:37 +0100

    work on new stuff

--- preprocess.py
+++ preprocess.py
@@ -4,47 +4,18 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 import metafeatures
 import features
+from constants import DIAG_CODES, TRAINING_FILES
 
 DATA_DIRECTORY = "/media/veracrypt2/data"
 #DATA_DIRECTORY = "data"
 DATA_DIRECTORY = "/volumes/NO NAME/data"
 
-DIAG_CODES = [
-    "D611",
-    "D642",
-    "D695",
-    "E032",
-    "E064",
-    "E160",
-    "E273",
-    "G211",
-    "G240",
-    "G251",
-    "G444",
-    "G620",
-    "I427",
-    "I952",
-    "L270",
-    "L271",
-    "M804",
-    "M814",
-    "N141",
-    "O355",
-    "R502",
-    "T599",
-    "T782",
-    "T783",
-    "T784",
-    "T789",
-    "T801",
-    "T802",
-    "T808",
-    "T809",
-    "T886",
-    "T887",
-]
+TEST_FILES = [x for x in DIAG_CODES if x not in TRAINING_FILES]
 
 path_for = "{0}/{1}-90-raw-measurements.csv".format
+
+BASE_MODEL_N_ESTIMATORS = 1
+META_MODEL_N_ESTIMATORS = 1
 
 
 def read_frame(diag_code):
@@ -66,7 +37,7 @@ def accuracy(y_hat, y_test):
     return corr / float(total)
 
 def build_base_model(xs, ys):
-    clf = RandomForestRegressor(n_estimators=1, n_jobs=-1, max_features=None)
+    clf = RandomForestRegressor(n_estimators=BASE_MODEL_N_ESTIMATORS, n_jobs=-1, max_features=None)
     clf.fit(xs, ys)
     return clf
 
@@ -107,7 +78,7 @@ def build_meta_model_xs(datasets):
 
 
 def build_meta_model(xs, ys):
-    clf = RandomForestRegressor(n_estimators=100, n_jobs=-1, max_features=None)
+    clf = RandomForestRegressor(n_estimators=META_MODEL_N_ESTIMATORS, n_jobs=-1, max_features=None)
     clf.fit(xs, ys)
     return clf
 
@@ -115,7 +86,7 @@ def meta_build_and_evaluate(xs, ys):
     x_train, x_test, y_train, y_test = train_test_split(xs, ys)
     clf = build_meta_model(x_train, y_train)
     y_hat = clf.predict(x_test)
-    return y_hat, y_train, ((y_hat - y_test) ** 2).mean()
+    return clf, y_hat, y_train, ((y_hat - y_test) ** 2).mean()
 
 
 def select_features_based_on_accuracy(accs):
@@ -125,7 +96,6 @@ def select_features_based_on_accuracy(accs):
 
 def get_meta_model_xs():
     try:
-        abc = 1/0
         return np.load("meta-model-xs.npy")
     except:
         data = [get_data_for(x) for x in TRAINING_FILES]
@@ -147,14 +117,11 @@ def get_meta_model_ys():
         return ys
 
 
-
-if __name__ == "__main__":
-    TRAINING_FILES = ["T887"]
+def meta_model_evaluation_run():
     xs = get_meta_model_xs()
     ys = get_meta_model_ys()
-
     print("Building meta model")
-    y_hat, y_train, meansquareerror = meta_build_and_evaluate(xs, ys)
+    clf, y_hat, y_train, meansquareerror = meta_build_and_evaluate(xs, ys)
     chosen = select_features_based_on_accuracy(y_hat)
     actual = select_features_based_on_accuracy(y_train)
     tot = 0
@@ -163,3 +130,57 @@ if __name__ == "__main__":
         if chosen == actual:
             tot += 1
     print tot
+
+
+def meta_model_train_all_run():
+    xs = get_meta_model_xs()
+    ys = get_meta_model_ys()
+    return build_meta_model(xs, ys)
+
+
+def pick_representation_for_ts(basedata, tsvar, clf):
+    metas = metafeatures.extract_meta_features_as_arr(basedata[tsvar])
+    return select_features_based_on_accuracy(clf.predict([metas]))[0]
+
+
+def make_smart_xs(basedata, clf):
+    feature_mapping = {
+        tsvar: pick_representation_for_ts(basedata, tsvar, clf)
+        for tsvar in basedata.columns.values
+    }
+    return features.df_to_features(basedata, feature_mapping, dict())
+
+
+def make_naive_xs(basedata):
+    feature_mapping = {k: True for k in basedata.columns.values}
+    return features.df_to_features(basedata, feature_mapping, dict())
+
+
+def get_experiments(clf, files):
+    for (basedata, ys) in [get_data_for(x) for x in files]:
+        smart_xs = make_smart_xs(basedata, clf)
+        naive_xs = make_navive_xs(basedata)
+        yield (naive_xs, smart_xs, ys)
+
+
+
+def generate_comparison(naive_results, smart_results):
+    return dict(naive=naive_results,
+                smart=smart_results,
+                delta=(naive_results - smart_results))
+
+
+def run_experiments(experiments):
+    for naive_xs, smart_xs, ys in experiments:
+        yield generate_comparison(build_and_evaluate(naive_xs, ys),
+                                  build_and_evaluate(smart_xs, ys))
+
+
+def run_evaluation_procedure():
+    clf = meta_model_train_all_run()
+    exprs = get_experiments(clf, TEST_FILES)
+    res = pd.DataFrame(list(run_experiments(exprs)))
+    res.to_csv("final-results.csv")
+
+if __name__ == "__main__":
+    run_evaluation_procedure()
